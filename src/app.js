aa3efc6b24df26881cfa1165bbea54f013688472
kassandra-mcp <kass.martindelcampo@gmail.com>
2020-09-05 17:15:33 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const exejs = require('ejs')
-const md5 = require('md5')
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const exejs = require('ejs');
+const md5 = require('md5');
 const nodemailer = require('nodemailer');
 
 const moment = require("moment");
@@ -13,22 +13,11 @@ require('dotenv').config({path: 'src/variables.env'});
 
 const app = express();
 
-//connecting to db
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_URL, 
-	{
-		useNewUrlParser: true, 
-		useUnifiedTopology: true
-	})
-
-const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 3000;
-
 // importando rutas
 const indexRoutes = require('./routes/index');
 
 //settings
-app.set("port", process.env.PORT || 3000);
+//app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", ".ejs");
 
@@ -40,6 +29,17 @@ app.use((req, res, next)=>{
     next();
   });
 
+//connecting to db
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DB_URL, 
+	{
+		useNewUrlParser: true, 
+		useUnifiedTopology: true
+	})
+
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 8080;
+
 //routers
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
@@ -49,7 +49,7 @@ app.use('/', indexRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 //starting the server
-app.listen(process.env.PORT || 3000, host, () => 
+app.listen(port, host, () => 
 {
 	console.log("Server on port");
 });
